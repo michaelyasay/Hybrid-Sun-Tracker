5e31779245511de5e70179ff07d913889f1ef0ea
michaelyasay <michaelyasay@users.noreply.github.com>
2016-09-28 18:15:14 +0800

    Update sunTracker.ino

--- sunTracker.ino
+++ sunTracker.ino
@@ -1,14 +1,19 @@
-#include <Servo.h>
-//defining Servos
-Servo servohori;
-int servoh = 0;
-int servohLimitHigh = 160;
-int servohLimitLow = 20;
+//Hybrid Sun Tracker System 1.0
+//09282016 - Created draft code based on Servo motor
+//         - added motor pins
 
-Servo servoverti; 
-int servov = 0; 
-int servovLimitHigh = 160;
-int servovLimitLow = 20;
+
+int hMotCounter = 0; 
+int hLimitHigh = 160;
+int hLimitLow = 20;
+
+int vMotCounter = 0; 
+int vLimitHigh = 160;
+int vLimitLow = 20;
+
+//Motor Pin Map
+int ENA=2, IN1=3, IN2=4, ENB=5, IN3=6, IN4=7
+
 //Assigning LDRs
 int ldrtopl = 2; //top left LDR green
 int ldrtopr = 1; //top right LDR yellow
@@ -17,18 +22,11 @@ int ldrbotr = 0; // bottom right LDR orange
 
  void setup () 
  {
-  servohori.attach(10);
-  servohori.write(0);
-  servoverti.attach(9);
-  servoverti.write(0);
-  delay(500);
+  Serial.begin(9600);
  }
 
 void loop()
 {
-  servoh = servohori.read();
-  servov = servoverti.read();
-
   //capturing analog values of each LDR
   int topl = analogRead(ldrtopl);	
   int topr = analogRead(ldrtopr);
@@ -43,48 +41,56 @@ void loop()
 
   if (avgtop < avgbot)			// bottom sensor is higher the top
   {
-    servoverti.write(servov +1);   	// move to look down
+    //move vertical motor forward
+    vMotorUp(); 
     if (servov > servovLimitHigh) 	// check for limit switch either replace this with limit switch with debounce
      { 
       servov = servovLimitHigh;		// flag to indicate already in the limit
      }
     delay(10);
   }
+ 
   else if (avgbot < avgtop)		// top sensor is higher that bottom
   {
-    servoverti.write(servov -1);	// move to look up
+    // move vertical motor backward
+    vMotorDown();
     if (servov < servovLimitLow)	// check for limit switch either replace this with limit switch with debounce
   {
     servov = servovLimitLow;		// flag to indicate already in the limit
   }
     delay(10);
   }
+ 
   else 
   {
-    servoverti.write(servov);		// this stops the vertical motor
+    vMotorStop();     // this stops the vertical motor
   }
   
   if (avgleft > avgright)		// left sensor is higher than right
   {
-    servohori.write(servoh +1);		// move to face right
+    // move horizontal motor to the right
+    hMotorRight();
     if (servoh > servohLimitHigh)	// check for limit switch either replace this with limit switch with debounce
     {
     servoh = servohLimitHigh;		// flag to indicate already in the limit
     }
     delay(10);
   }
+ 
   else if (avgright > avgleft)		// right sensor is higher than left
   {
-    servohori.write(servoh -1);		// move to face left
+    // move horizontal motor to the left
+    hMotorleft();
     if (servoh < servohLimitLow)	// check for limit switch either replace this with limit switch with debounce
      {
      servoh = servohLimitLow;		// flag to indicate already in the limit
      }
     delay(10);
   }
+ 
   else 
   {
-    servohori.write(servoh);		// this stops the horizontal motor
+    hMotorStop(); // this stops the horizontal motor
   }
   delay(50);
 }
